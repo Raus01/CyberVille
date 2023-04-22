41579ab27b3291e5f9469278260fc4557f0fcdc7
Raus01 <rauslauri7@gmail.com>
2023-04-22 11:50:51 +0300

    koik

--- main.go
+++ main.go
@@ -13,12 +13,16 @@ import (
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
+func dash(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "dashboard.html")
+}
 
 func main() {
 	fs := http.FileServer(http.Dir("css"))
 	http.Handle("/css/", http.StripPrefix("/css/", fs))
-	
+
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/dash", dash)
 	log.Println("Server started on: http://localhost:8000")
 	//err := http.ListenAndServe(":8000", context.ClearHandler(http.DefaultServeMux)) // context to prevent memory leak
 	err := http.ListenAndServe(":8000", nil)
